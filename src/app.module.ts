9802e6f123158b0af8d4434546d3b87a852e3d0c
Darshan Patel <darshanpatel2608ce@gmail.com>
2025-02-08 16:45:37 +0530

    db connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { IdentityMiddleware } from './identity/identity.middleware';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
+// url: "postgresql://postgres:Darshan#260802@db.ytyhbijqjwogkptqffhb.supabase.co:5432/postgres"
 @Module({
-  imports: [AuthModule],
+  imports: [ConfigModule.forRoot(), AuthModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: 5432,
+    username: 'postgres',
+    password: process.env.DB_PASSWORD,
+    database: 'postgres',
+    entities: [],
+    synchronize: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
