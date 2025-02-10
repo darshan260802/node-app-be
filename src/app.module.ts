4abb1fe664e5f2c01342edd341e42c77505a4515
Darshan Patel <darshanpatel2608ce@gmail.com>
2025-02-10 19:45:32 +0530

    restructure

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,11 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { IdentityMiddleware } from './identity/identity.middleware';
+import { IdentityMiddleware } from './middlewares/identity/identity.middleware';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { User } from './entities/user.identity';
-import { PublicIpMiddleware } from './public-ip/public-ip.middleware';
+import { PublicIpMiddleware } from './middlewares/public-ip/public-ip.middleware';
 
 // url: "postgresql://postgres:Darshan#260802@db.ytyhbijqjwogkptqffhb.supabase.co:5432/postgres"
 @Module({
