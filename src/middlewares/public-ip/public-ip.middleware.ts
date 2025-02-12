3329e500b3c47e0dee738ab2725c2cab854aaea6
Darshan Patel <darshanpatel2608ce@gmail.com>
2025-02-12 08:41:48 +0530

    asd

--- src/middlewares/public-ip/public-ip.middleware.ts
+++ src/middlewares/public-ip/public-ip.middleware.ts
@@ -7,7 +7,7 @@ export class PublicIpMiddleware implements NestMiddleware {
     // const publicIP = "122.170.24.97";
     const publicIP = (""+req.headers['x-forwarded-for'] || '').split(',')[0];
 
-    console.info('DUMP: ->', publicIP, req.headers['user-agent'], req.socket.remoteFamily, req.socket.remotePort);
+    console.info('DUMP: ->', publicIP, req.headers['user-agent'], req.socket.remoteFamily, req.headers);
     
     if(!publicIP || publicIP === 'undefined') {
       throw new HttpException('Invalid Identity', HttpStatus.UNAUTHORIZED);
